bc5c593d5bba16c5504e5a9421c98259961ee758
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-06-08 19:17:12 +0300

    Добавляет решения 003 - 014
    Добавляет решения 003 - 014

--- js/js-homework.js
+++ js/js-homework.js
@@ -709,43 +709,229 @@ console.log(message);
 // console.log(aptPrice);
 // console.log(aptTags);
 // ---------------------------------- Module 003 Task 004
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+// // Change code above this line
+// console.log(ownerName);
+// console.log(ownerPhone);
+// console.log(ownerEmail);
+// console.log(numberOfTags);
+// console.log(firstTag);
+// console.log(lastTag);
+// // Change code above this line
+// ---------------------------------- Module 003 Task 005
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
+// // Change code below this line
+// //
+// const aptRating = apartment['rating'];
+// const aptDescr = apartment['descr'];
+// const aptPrice = apartment['price'];
+// const aptTags = apartment['tags'];
+// // Change code above this line
+// console.log(aptRating);
+// console.log(aptDescr);
+// console.log(aptPrice);
+// console.log(aptTags);
+// ---------------------------------- Module 003 Task 006
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = 'Henry Sibola';
+// apartment.tags.push('trusted');
+// // Change code below this line
+// console.log(apartment);
+// ---------------------------------- Module 003 Task 007
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ['premium', 'promoted', 'top', 'trusted'],
+//   owner: {
+//     name: 'Henry Sibola',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {};
+// apartment.location.country = 'Jamaica';
+// apartment.location.city = 'Kingston';
+
+// // Change code below this line
+// console.log(apartment);
+// ---------------------------------- Module 003 Task 008
+// const name = 'Repair Droid';
+// const price = 2500;
+// const image = 'https://via.placeholder.com/640x480';
+// const tags = ['on sale', 'trending', 'best buy'];
+
+// const product = {
+//   // Change code below this line
+//   name,
+//   price,
+//   image,
+//   tags,
+
+//   // Change code above this line
+// };
+// console.log(product);
+// ---------------------------------- Module 003 Task 009
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
+
+// const credentials = {
+//   // Change code below this line
+//   [emailInputName]: 'henry.carter@aptmail.com',
+//   [passwordInputName]: 'jqueryismyjam',
+
+//   // Change code above this line
+// };
+// console.log(credentials);
+// ---------------------------------- Module 003 Task 010
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+// ---------------------------------- Module 003 Task 011
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: 'apt',
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = 'Spacious apartment in the city center';
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+//   // Change code above this line
+// }
+// console.log(apartment);
+// console.log(keys);
+// console.log(values);
+// ---------------------------------- Module 003 Task 012
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ['premium', 'promoted', 'top', 'trusted'],
+//   owner: {
+//     name: 'Henry Sibola',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
+//   for (const key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     }
+//   }
+//   // Change code above this line
+//   return propCount;
+// }
+
+// console.log(countProps(apartment));
+// ---------------------------------- Module 003 Task 013
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+// console.log(values);
+// console.log(keys);
+// console.log(apartment);
+// ---------------------------------- Module 003 Task 014
 const apartment = {
-  imgUrl: 'https://via.placeholder.com/640x480',
   descr: 'Spacious apartment in the city center',
   rating: 4,
   price: 2153,
-  tags: ['premium', 'promoted', 'top'],
-  owner: {
-    name: 'Henry',
-    phone: '982-126-1588',
-    email: 'henry.carter@aptmail.com',
-  },
 };
+function countProps(object) {
+  // Change code below this line
+  //let propCount = 0;
 
-// Change code below this line
-const ownerName = apartment.owner.name;
-const ownerPhone = apartment.owner.phone;
-const ownerEmail = apartment.owner.email;
-const numberOfTags = apartment.tags.length;
-const firstTag = apartment.tags[0];
-const lastTag = apartment.tags[apartment.tags.length];
-console.log(ownerName);
-console.log(ownerPhone);
-console.log(ownerEmail);
-console.log(numberOfTags);
-console.log(firstTag);
-console.log(lastTag);
-// Change code above this line
-// ---------------------------------- Module 003 Task 005
-// ---------------------------------- Module 003 Task 006
-// ---------------------------------- Module 003 Task 007
-// ---------------------------------- Module 003 Task 008
-// ---------------------------------- Module 003 Task 009
-// ---------------------------------- Module 003 Task 010
-// ---------------------------------- Module 003 Task 011
-// ---------------------------------- Module 003 Task 012
-// ---------------------------------- Module 003 Task 013
-// ---------------------------------- Module 003 Task 014
+  //for (const key in object) {
+  //  if (object.hasOwnProperty(key)) {
+  //   propCount += 1;
+  //  }
+  //}
+
+  //return propCount;
+
+  for (const key of Object.keys(object)) {
+  }
+
+  return Object.keys(object).length;
+  // Change code above this line
+}
+console.log(countProps(apartment));
 // ---------------------------------- Module 003 Task 015
 // ---------------------------------- Module 003 Task 016
 // ---------------------------------- Module 003 Task 017

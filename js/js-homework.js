b4e051f603dd81b23f5499db7d941d27e5d00c26
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-06-10 19:36:26 +0300

    Добавляет решения 029 - 003
    Добавляет решения 029 - 003

--- js/js-homework.js
+++ js/js-homework.js
@@ -966,7 +966,6 @@ console.log(message);
 //   { name: 'Droid', price: 400, quantity: 7 },
 //   { name: 'Grip', price: 1200, quantity: 9 },
 // ];
-
 // function getProductPrice(productName) {
 //   // Change code below this line
 //   for (const product of products) {
@@ -975,10 +974,8 @@ console.log(message);
 //     }
 //   }
 //   return null;
-
 //   // Change code above this line
 // }
-
 // console.log(getProductPrice('Radar'));
 // console.log(getProductPrice('Grip'));
 // console.log(getProductPrice('Scanner'));
@@ -1041,12 +1038,10 @@ console.log(message);
 //   tomorrow: 33,
 // };
 // // Change code below this line
-
 // //const yesterday = highTemperatures.yesterday;
 // //const today = highTemperatures.today;
 // //const tomorrow = highTemperatures.tomorrow;
 // const { yesterday, today, tomorrow } = highTemperatures;
-
 // // Change code above this line
 // const meanTemperature = (yesterday + today + tomorrow) / 3;
 // ---------------------------------- Module 003 Task 022
@@ -1056,24 +1051,128 @@ console.log(message);
 //   tomorrow: 33,
 // };
 // // Change code below this line
-
 // const {
 //   yesterday,
 //   today,
 //   tomorrow,
 //   icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
 // } = highTemperatures;
-
 // // Change code above this line
 // const meanTemperature = (yesterday + today + tomorrow) / 3;
 // ---------------------------------- Module 003 Task 023
-
+// /const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+//   icon: "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"
+// };
+// // Change code below this line
+// //const highYesterday = highTemperatures.yesterday;
+// //const highToday = highTemperatures.today;
+// //const highTomorrow = highTemperatures.tomorrow;
+// //const highIcon = highTemperatures.icon;
+// const {yesterday: highYesterday,
+//        today: highToday,
+//        tomorrow: highTomorrow,
+//        icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg", } = highTemperatures;
+// // Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
 // ---------------------------------- Module 003 Task 024
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const { hex, rgb } of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
 // ---------------------------------- Module 003 Task 025
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+
+// //const highToday = forecast.today.high;
+// //const lowToday = forecast.today.low;
+// //const todayIcon = forecast.today.icon;
+
+// //const highTomorrow = forecast.tomorrow.high;
+// //const lowTomorrow = forecast.tomorrow.low;
+// //const tomorrowIcon = forecast.tomorrow.icon;
+
+// const {
+//   today: {
+//     low: lowToday,
+//     high: highToday,
+//     icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+//   },
+//   tomorrow: {
+//     low: lowTomorrow,
+//     high: highTomorrow,
+//     icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+//   },
+// } = forecast;
 // ---------------------------------- Module 003 Task 026
+// function calculateMeanTemperature(forecast) {
+//   //const todayLow = forecast.today.low;
+//   //const todayHigh = forecast.today.high;
+//   //const tomorrowLow = forecast.tomorrow.low;
+//   //const tomorrowHigh = forecast.tomorrow.high;
+
+//   const {
+//     today: { low: todayLow, high: todayHigh },
+//     tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+//   } = forecast;
+//   // Change code above this line
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
 // ---------------------------------- Module 003 Task 027
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
 // ---------------------------------- Module 003 Task 028
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [
+//   ...firstGroupScores,
+//   ...secondGroupScores,
+//   ...thirdGroupScores,
+// ];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
 // ---------------------------------- Module 003 Task 029
+// const defaultSettings = {
+//   theme: 'light',
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// // Change code below this line
+// const finalSettings = { ...defaultSettings, ...overrideSettings };
 // ---------------------------------- Module 003 Task 030
 // ---------------------------------- Module 003 Task 031
 // ---------------------------------- Module 003 Task 032

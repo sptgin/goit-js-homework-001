5813f5f62c1a65b678b3ba9ad2ad9aeeac7642d8
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-06-12 16:49:13 +0300

    Добавляет решения  006 - 004
    Добавляет решения  006 - 004

--- js/js-homework.js
+++ js/js-homework.js
@@ -1358,14 +1358,119 @@ console.log(message);
 // const result = makePizza();
 // const pointer = makePizza;
 // ---------------------------------- Module 004 Task 002
+// function deliverPizza(pizzaName) {
+//   return `Доставляем пиццу ${pizzaName}.`;
+// }
 
+// function makePizza(pizzaName) {
+//   return `Пицца ${pizzaName} готовится, ожидайте...`;
+// }
+
+// // Пиши код ниже этой строки
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+// console.log(makeMessage('Роял гранд', makePizza));
+// console.log(makeMessage('Ультрасыр', deliverPizza));
 // ---------------------------------- Module 004 Task 003
+// function makePizza(pizzaName, callback) {
+//   console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
+//   callback(pizzaName);
+// }
+
+// makePizza('Роял гранд', function deliverPizza(pizzaName) {
+//   console.log(`Доставляем пиццу ${pizzaName}.`);
+// });
+// // Пиши код ниже этой строки
+
+// makePizza('Ультрасыр', function eatPizza(pizzaName) {
+//   console.log(`Едим пиццу ${pizzaName}`);
+// });
 // ---------------------------------- Module 004 Task 004
+// const pizzaPalace = {
+//   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+//   order(pizzaName, onSuccess, onError) {
+//     for (const pizza of this.pizzas) {
+//       if (pizza == pizzaName) {
+//         return onSuccess(pizzaName);
+//       }
+//     }
+//     return onError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
+//   },
+// };
+// // Пиши код выше этой строки
+
+// // Колбэк для onSuccess
+// function makePizza(pizzaName) {
+//   return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+// }
+
+// // Колбэк для onError
+// function onOrderError(error) {
+//   return `Ошибка! ${error}`;
+// }
+
+// // Вызовы метода с колбэками
+// console.log(pizzaPalace.order('Аль Копчино', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Четыре нарезона', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Биг майк', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Венская', makePizza, onOrderError));
 // ---------------------------------- Module 004 Task 005
+// const pizzaPalace = {
+//   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+//   // Пиши код ниже этой строки
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+//     }
+
+//     return `Заказ принят, готовим пиццу «${pizzaName}».`;
+//   },
+//   // Пиши код выше этой строки
+// };
 // ---------------------------------- Module 004 Task 006
+// const customer = {
+//   username: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['Burger', 'Pizza', 'Salad'],
+//   // Пиши код ниже этой строки
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+//   // Пиши код выше этой строки
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, 'Steak');
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ['Burger', 'Pizza', 'Salad', 'Steak']
 // ---------------------------------- Module 004 Task 007
+
 // ---------------------------------- Module 004 Task 008
+
 // ---------------------------------- Module 004 Task 009
+
 // ---------------------------------- Module 004 Task 010
+
 // ------------------------------------------------------
 console.log('JS - The End.');

4320f9856335d4d2eefa8b1dac7b8be10afc543c
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-06-15 18:54:09 +0300

    Добавляет решения 010 - 004

--- js/js-homework.js
+++ js/js-homework.js
@@ -1502,8 +1502,61 @@ console.log(message);
 //   messages.push(msg);
 // }
 // ---------------------------------- Module 004 Task 009
+// const pizzaPalace = {
+//   company: 'Pizza Palace',
+// };
 
+// const burgerShack = {
+//   company: 'Burger Shack',
+// };
+
+// function composeMessage(customerName) {
+//   return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+// }
+// // Пиши код ниже этой строки
+
+// const pizzaPalaceComposer = composeMessage.bind(pizzaPalace);
+// const pizzaPalaceMessage = pizzaPalaceComposer('Манго');
+
+// const burgerShackComposer = composeMessage.bind(burgerShack);
+// const burgerShackMessage = burgerShackComposer('Поли');
 // ---------------------------------- Module 004 Task 010
+// const service = {
+//   mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+//   subscribe(email) {
+//     this.mailingList.push(email);
+//     return `Почта ${email} добавлена в рассылку.`;
+//   },
+//   unsubscribe(email) {
+//     this.mailingList = this.mailingList.filter(e => e !== email);
+//     return `Почта ${email} удалена из рассылки.`;
+//   },
+// };
 
+// function logAndInvokeAction(email, action) {
+//   console.log(`Выполняем действие с ${email}.`);
+//   return action(email);
+// }
+
+// const firstInvoke = logAndInvokeAction(
+//   'kiwi@mail.uk',
+//   service.subscribe.bind(service),
+// );
+// console.log(firstInvoke);
+// // Почта kiwi@mail.uk добавлена в рассылку.
+
+// console.log(service.mailingList);
+// /* ['mango@mail.com',
+//     'poly@hotmail.de',
+//     'ajax@jmail.net',
+//     'kiwi@mail.uk']*/
+// const secondInvoke = logAndInvokeAction(
+//   'poly@hotmail.de',
+//   service.unsubscribe.bind(service),
+// );
+// console.log(secondInvoke);
+// // Почта poly@hotmail.de удалена из рассылки.
+
+// console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
 // ------------------------------------------------------
 console.log('JS - The End.');

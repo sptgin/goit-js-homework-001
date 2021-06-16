67b97b4f497d05fa26d062f31200bb17d05146a8
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-06-16 16:07:51 +0300

    Добавляет решения 016 - 005
    Добавляет решения 016 - 005

--- js/js-homework.js
+++ js/js-homework.js
@@ -1640,25 +1640,212 @@ console.log(message);
 // storage.removeItem('Пролонгер');
 // console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
 // ---------------------------------- Module 005 Task 007
+// function StringBuilder(baseValue) {
+//   this.value = baseValue;
+// }
+// StringBuilder.prototype.getValue = function () {
+//   return this.value;
+// };
+// StringBuilder.prototype.padEnd = function (str) {
+//   return (this.value = this.value + str);
+// };
+// StringBuilder.prototype.padStart = function (str) {
+//   return (this.value = str + this.value);
+// };
+// StringBuilder.prototype.padBoth = function (str) {
+//   return (this.value = str + this.value + str);
+// };
 
+// // Пиши код выше этой строки
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // '.'
+// builder.padStart('^');
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd('^');
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth('=');
+// console.log(builder.getValue()); // '=^.^='
 // ---------------------------------- Module 005 Task 008
-
+// class Car {};
 // ---------------------------------- Module 005 Task 009
+// class Car {
+//   brand;
+//   model;
+//   price;
 
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
 // ---------------------------------- Module 005 Task 010
-
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getPrice() {
+//     return this.price;
+//   }
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+// }
 // ---------------------------------- Module 005 Task 011
-
+// class Car {
+//   #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getBrand() {
+//     return this.#brand;
+//   }
+//   changeBrand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+// }
 // ---------------------------------- Module 005 Task 012
-
+// class Storage {
+//   #items;
+//   constructor(items) {
+//     this.#items = items;
+//   }
+//   getItems() {
+//     return this.#items;
+//   }
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+//   removeItem(item) {
+//     const itemIndex = this.#items.indexOf(item);
+//     this.#items.splice(itemIndex, 1);
+//   }
+// }
+// // Пиши код выше этой строки
+// const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem('Дроид');
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem('Пролонгер');
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
 // ---------------------------------- Module 005 Task 013
-
+// class StringBuilder {
+//   #value;
+//   constructor(baseValue) {
+//     this.#value = baseValue;
+//   }
+//   getValue() {
+//     return this.#value;
+//   }
+//   padEnd(str) {
+//     this.#value += str;
+//   }
+//   padStart(str) {
+//     this.#value = str + this.#value;
+//   }
+//   padBoth(str) {
+//     this.padStart(str);
+//     this.padEnd(str);
+//   }
+// }
+// // Пиши код выше этой строки
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // '.'
+// builder.padStart('^');
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd('^');
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth('=');
+// console.log(builder.getValue()); // '=^.^='
 // ---------------------------------- Module 005 Task 014
+// class Car {
+//   #model;
+//   #price;
+//   #brand;
 
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+//   set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+// }
 // ---------------------------------- Module 005 Task 015
+// class Car {
+//   // Пиши код ниже этой строки
+//   static MAX_PRICE = 50000;
+//   #price;
 
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (newPrice <= Car.MAX_PRICE) {
+//       this.#price = newPrice;
+//     }
+//   }
+//   // Пиши код выше этой строки
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
 // ---------------------------------- Module 005 Task 016
-
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Пиши код ниже этой строки
+//   static checkPrice(price) {
+//     if (price > this.#MAX_PRICE) {
+//       return 'Внимание! Цена превышает допустимую.';
+//     }
+//     return 'Всё хорошо, цена в порядке.';
+//   }
+//   // Пиши код выше этой строки
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+// console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
+// console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.
 // ---------------------------------- Module 005 Task 017
 
 // ---------------------------------- Module 005 Task 018
